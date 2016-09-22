da186e67543e893b6380a5518aff46e66b5c2cf9
supermancbs <clark.sacktor@gmail.com>
2016-09-22 14:27:06 -0400

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
   has_one :account
   has_many :funds, through: :account
+  validates :email, uniqueness: true
+  validates :first_name, :last_name, :password, :email, :address, presence: true
 
   has_secure_password
 
   def name
     "#{self.first_name} #{self.last_name}"
   end
+
 end
