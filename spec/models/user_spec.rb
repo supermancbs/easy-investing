2f0a42b7235557b0718c89a9e9be0292a9f4c02c
supermancbs <clark.sacktor@gmail.com>
2016-09-22 13:02:27 -0400

    full name user method works

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,13 +9,19 @@ RSpec.describe User, type: :model do
     end
   end
 
+  context "full name" do
+    it "user has full name" do
+      clark = User.create!(first_name: "Clark", last_name: "Sacktor", password: "password", email:"clark@clark.com", address: "123 main st")
+      expect(clark.name).to eq("Clark Sacktor")
+    end
+  end
+
   context "user has many funds" do
     it "can have many funds through accounts" do
       clark = User.create!(first_name: "Clark", last_name: "Sacktor", password: "password", email:"clark@clark.com", address: "123 main st")
       account = Account.create!(user:clark, balance: 100)
       s_p = account.funds << Fund.create!(name: "S&P 500")
-      dow_jones = account.funds << Fund.create!(name: "Dow Jones")
-      expect(clark.funds).to eq([s_p, dow_jones])
+      expect(clark.funds).to eq(s_p)
     end
   end
 end
